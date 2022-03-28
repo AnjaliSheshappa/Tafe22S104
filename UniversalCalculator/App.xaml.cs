29c8288c4d364fc76f96a96474558004a6d4a277
Zhenye Ling <leungkeno@gmail.com>
2022-03-28 15:43:58 +1030

    conflicts the code

--- UniversalCalculator/App.xaml.cs
+++ UniversalCalculator/App.xaml.cs
@@ -30,12 +30,12 @@ namespace Calculator
 			if (e.PrelaunchActivated == false)
 			{
 				if (rootFrame.Content == null)
-<<<<<<< HEAD
-					rootFrame.Navigate(typeof(Currency_Calculator_Page), e.Arguments);
-=======
+
+					//rootFrame.Navigate(typeof(Currency_Calculator_Page), e.Arguments);
+
 					rootFrame.Navigate(typeof(mortgage), e.Arguments);
->>>>>>> 2f8cec87b6963f839ee7d8ea6caa8fc8021f9c4d
-					rootFrame.Navigate(typeof(hwshoppingcart), e.Arguments);
+
+					//rootFrame.Navigate(typeof(hwshoppingcart), e.Arguments);
 
 				Window.Current.Activate();
 			}
